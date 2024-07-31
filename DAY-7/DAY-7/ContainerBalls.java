3902dbf620ba72b39eb9ccdedcb5b3aaeabeda10
Nathani Varshith <123011598+varshithnathani@users.noreply.github.com>
2024-07-31 10:59:29 +0530

    Add files via upload

---
+++ DAY-7/DAY-7/ContainerBalls.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class ContainerBalls{
+    public static void main(String[] args){
+        Scanner SC = new Scanner(System.in);
+        int n = SC.nextInt();
+        System.out.println(solve(n));
+    }
+    public static int solve(int n){
+        // if(n==1)
+        //     return 1;
+        // if(n==2)
+        //     return 2;
+        // if(n==3)
+        //     return 3;
+        // int[] arr = new int[n+1];
+        // arr[1] = 1;
+        // arr[2] = 1;
+        // arr[3] = 2;
+        // for(int i=4; i<=n; i++){
+        //     arr[i] = arr[i-1]+arr[i-3];
+        // }
+        // return arr[n];
+        
+        if(n==1 || n ==2)
+            return 1;
+        if(n==3)
+            return 2;
+        else{
+            return solve(n-3)+solve(n-1);
+        }
+        
+        
+    }
+}
