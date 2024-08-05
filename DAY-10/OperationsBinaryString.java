899e0c266a7dbb3ceae29a1dbd9176bec3105b77
Nathani Varshith <123011598+varshithnathani@users.noreply.github.com>
2024-08-05 20:38:08 +0530

    Add files via upload

---
+++ DAY-10/OperationsBinaryString.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class OperationsBinaryString {
+    public static void main(String[] args) {
+        Scanner SC = new Scanner(System.in);
+        String str = SC.next();
+        System.out.println(solve(str));
+    }
+    public static int solve(String str){
+        int val = str.charAt(0)-'0';
+        for(int i=1; i<str.length();){
+            char ch = str.charAt(i);
+            i++;
+            if(ch == 'A'){
+                val = val & (str.charAt(i)-'0');
+            }else if(ch == 'B'){
+                val = val | (str.charAt(i)-'0');
+            }
+            else{
+                val = val ^ (str.charAt(i)-'0');
+            }
+            i++;
+        }
+        return val;
+
+    }
+}
