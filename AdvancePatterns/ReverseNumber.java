db4d9a634d1994164bb8aceda4f7501ac1d50118
Nathani Varshith <123011598+varshithnathani@users.noreply.github.com>
2024-08-29 10:53:59 +0530

    Add files via upload

---
+++ AdvancePatterns/ReverseNumber.java
@@ -0,0 +1,13 @@
+package AdvancePatterns;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i =n; i>= 0; i--){
+            for(int j =1; j<= i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
